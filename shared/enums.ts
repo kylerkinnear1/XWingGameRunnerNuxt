7d9af307fa7901585037849eb2d9e931605997b7
Kyle Kinnear <kylerkinnear@gmail.com>
2025-12-20 23:32:00 -0600

    Adding in game state objects. will just do client side first, then add server side to store state after each step, or going back a step.

--- shared/enums.ts
+++ shared/enums.ts
@@ -9,3 +9,86 @@ export const factionOptions = [
     { value: Faction.Scum, label: 'Scum and Villainy' },
     { value: Faction.Empire, label: 'Galactic Empire' }
 ] as const;
+
+export enum Bearing {
+  Straight = 'straight',
+  BankLeft = 'bank-left',
+  BankRight = 'bank-right',
+  TurnLeft = 'turn-left',
+  TurnRight = 'turn-right',
+  KTurn = 'k-turn',
+  SloopLeft = 'sloop-left',
+  SloopRight = 'sloop-right',
+  TallonRollLeft = 'tallon-roll-left',
+  TallonRollRight = 'tallon-roll-right',
+  Stationary = 'stationary',
+  Reverse = 'reverse'
+}
+
+export enum ManeuverDifficulty {
+  Green = 'green',
+  White = 'white',
+  Red = 'red'
+}
+
+export enum TokenType {
+  Focus = 'focus',
+  Evade = 'evade',
+  Stress = 'stress',
+  Ion = 'ion',
+  TargetLock = 'target-lock',
+  Reinforce = 'reinforce',
+  Cloak = 'cloak',
+  Jam = 'jam',
+  Calculate = 'calculate',
+  Charge = 'charge',
+  Force = 'force',
+  Disarm = 'disarm',
+  Tractor = 'tractor',
+  Strain = 'strain',
+  Deplete = 'deplete',
+  Shield = 'shield',
+  Energy = 'energy',
+  WeaponsDisabled = 'weapons-disabled',
+  Condition = 'condition'
+}
+
+export enum ActionType {
+  Focus = 'focus',
+  Evade = 'evade',
+  TargetLock = 'target-lock',
+  BarrelRoll = 'barrel-roll',
+  Boost = 'boost',
+  Reinforce = 'reinforce',
+  Coordinate = 'coordinate',
+  Jam = 'jam',
+  Calculate = 'calculate',
+  Cloak = 'cloak',
+  SLAM = 'slam',
+  Reload = 'reload',
+  RotateArc = 'rotate-arc',
+  Card = 'card'
+}
+
+export enum DiceResult {
+  Hit = 'hit',
+  Crit = 'crit',
+  Focus = 'focus',
+  Evade = 'evade',
+  Blank = 'blank'
+}
+
+export enum CollisionType {
+  Asteroid = 'asteroid',
+  Debris = 'debris',
+  GasCloud = 'gas-cloud',
+  Mine = 'mine',
+  Ship = 'ship'
+}
+
+// Maneuver helper type
+export interface Maneuver {
+  bearing: Bearing
+  speed: number
+  difficulty: ManeuverDifficulty
+}
