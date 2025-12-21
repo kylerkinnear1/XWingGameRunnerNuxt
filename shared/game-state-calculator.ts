09c41423bafca5ae0aa1d98a861c82bb0d33bc3e
Kyle Kinnear <kylerkinnear@gmail.com>
2025-12-21 00:14:17 -0600

    Stubbed out game state calculator.

---
+++ shared/game-state-calculator.ts
@@ -0,0 +1,8 @@
+import type { Maneuver, TokenType, ActionType, DiceResult, CollisionType, ReinforceDirection, GamePhase } from '#shared/enums'
+import type { SquadReadDto, SquadReadResponseDto, SquadUpdateDto, SquadUpdateResponseDto, PilotDto as SquadPilotDto } from '#shared/squad-dto'
+import type { CardsDto, PilotDto as CardPilotDto, UpgradeDto } from '#shared/cards'
+import type { GameStateDto, CurrentGameState, ShipStateDto, CritStateDto, TokenStateDto, WeaponStateDto, GameStepDto } from '#shared/game-state-dto'
+
+const calculateGameState = (gameState: GameStateDto, player1Squad: SquadReadDto, player2Squad: SquadReadDto): CurrentGameState => {
+    throw new Error('Not implemented')
+}
